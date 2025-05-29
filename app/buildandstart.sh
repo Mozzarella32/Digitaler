a95ef4470caaa00575ad913eaeca018c219e19ec
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2025-05-29 20:13:36 +0200

    fixed cmake typo

--- app/buildandstart.sh
+++ app/buildandstart.sh
@@ -6,5 +6,5 @@ if [ ! -d "../build" ]; then
   cmake ../
   cd ../app
 fi
-cmake --build ../build -j$(nproc) || cmkae -S ../ -B ../build && cmake --build ../build -j$(nproc) 
+cmake --build ../build -j$(nproc) || cmake -S ../ -B ../build && cmake --build ../build -j$(nproc) 
 ./Digitaler
