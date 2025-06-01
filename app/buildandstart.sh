0578f51f33495240d97585104a16c9ecc0e9de98
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2025-06-01 10:00:02 +0200

    Corrected buildandstart.sh(again)

--- app/buildandstart.sh
+++ app/buildandstart.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
+rm Digitaler
+rm Error.log
 if [ ! -d "../build" ]; then
   mkdir ../build
   cd ../build
   cmake ../
   cd ../app
 fi
-cmake --build ../build -j$(nproc) || cmake -S ../ -B ../build && cmake --build ../build -j$(nproc) 
+cmake --build ../build -j$(nproc) || (cmake -S ../ -B ../build && cmake --build ../build -j$(nproc)) 
 ./Digitaler
