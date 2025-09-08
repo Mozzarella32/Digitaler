0d2611443f50e97cf30f859b28f1193dd0885877
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2025-09-08 18:38:11 +0000

    Change DrawBuffers again

--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -61,8 +61,8 @@ private:
 	Texture FBOBlurMarkedStencileDepthTexture;
 	FrameBufferObject FBOBlurMarked;
 
-	std::array<GLenum, 1> DrawBuffer0 = { GL_COLOR_ATTACHMENT0 };
-	std::array<GLenum, 2> DrawBuffer1 = { GL_COLOR_ATTACHMENT1 };
+	std::array<GLenum, 2> DrawBuffer0 = { GL_COLOR_ATTACHMENT0, GL_NONE };
+	std::array<GLenum, 2> DrawBuffer1 = { GL_NONE, GL_COLOR_ATTACHMENT1 };
 
 	struct PathVAOs {
 		VertexArrayObject EdgesVAO;
